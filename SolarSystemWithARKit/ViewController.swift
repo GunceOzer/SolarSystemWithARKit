e50fccf791173b5dcca83d9c9e49c892e5528703
Günce Demirel Özer <gunce3@hotmail.com>
2022-09-28 17:51:23 +0200

    ARKit

--- SolarSystemWithARKit/ViewController.swift
+++ SolarSystemWithARKit/ViewController.swift
@@ -16,17 +16,46 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+    
+        let sun = createSphere(radius: 0.3, content: "sun.jpeg", vector: SCNVector3(0,0.2,-1))
+        let mercury = createSphere(radius: 0.2, content: "mercury.jpeg", vector: SCNVector3(0.5,0.2,-1))
+        let venus = createSphere(radius: 0.25, content: "venus.png", vector: SCNVector3(1.0,0.2,-1))
+        let earth = createSphere(radius: 0.3, content: "earth.jpeg", vector: SCNVector3(1.5,0.2,-1))
+        let mars = createSphere(radius: 0.1, content: "mars.png", vector: SCNVector3(2,0.2,-1))
+        let jupiter = createSphere(radius: 0.5, content: "jupiter.jpeg", vector: SCNVector3(2.5,0.2,-1))
+        let saturn = createSphere(radius: 0.4, content: "saturn.jpeg", vector: SCNVector3(3,0.2,-1))
+        let uranus = createSphere(radius: 0.27, content: "uranus.jpeg", vector: SCNVector3(3.5,0.2,-1))
+        let neptune = createSphere(radius: 0.3, content: "neptune.jpeg", vector: SCNVector3(4,0.2,-1))
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.scene.rootNode.addChildNode(sun)
+        sceneView.scene.rootNode.addChildNode(mercury)
+        sceneView.scene.rootNode.addChildNode(venus)
+        sceneView.scene.rootNode.addChildNode(earth)
+        sceneView.scene.rootNode.addChildNode(mars)
+        sceneView.scene.rootNode.addChildNode(jupiter)
+        sceneView.scene.rootNode.addChildNode(saturn)
+        sceneView.scene.rootNode.addChildNode(uranus)
+        sceneView.scene.rootNode.addChildNode(neptune)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    func createSphere(radius : CGFloat , content : String , vector: SCNVector3)->SCNNode{
+        
+        let mySphere = SCNSphere(radius: radius)
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIImage(named: "art.scnassets/\(content)")
+        
+                                                  
+        mySphere.materials = [sphereMaterial]
+        let node = SCNNode()
+        node.position = vector
+        node.geometry = mySphere
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        return node
     }
     
     override func viewWillAppear(_ animated: Bool) {
